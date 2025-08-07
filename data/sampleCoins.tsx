6abe536c912e5b88c9426a0222556d6fcdf9f487
Eyewritecode <933kss@gmail.com>
2025-08-08 02:50:32 +0900

    update mock data

--- data/sampleCoins.tsx
+++ data/sampleCoins.tsx
@@ -3,42 +3,47 @@ import { Memecoin } from "@/interfaces/memecoin";
 export const sampleCoins: Memecoin[] = [
   {
     name: "PENGU",
-    ticker: "PENGU",
-    marketCap: "2.6B",
+    symbol: "PENGU",
+    marketcap: "2.6",
     change: 2,
-    imageUrl: "/pengu.jpg",
-    dexUrl: "https://dexscreener.com/solana/4r2agqrjulpgl9o1vuw1mkcvhve4dti7yzjhqghj2qer"
+    price: 324141,
+    logoURI: "/pengu.jpg",
+    address: "https://dexscreener.com/solana/4r2agqrjulpgl9o1vuw1mkcvhve4dti7yzjhqghj2qer"
   },
   {
     name: "USELESS",
-    ticker: "USELESS",
-    marketCap: "988M",
+    symbol: "USELESS",
+    marketcap: "988",
     change: 7,
-    imageUrl: "/useless.jpg",
-    dexUrl: "https://dexscreener.com/solana/8jzua3cmwd8dbwxcyp1d9nnsonwqbrh8jstwk2gfpwja"
+    price: 324141,
+    logoURI: "/useless.jpg",
+    address: "https://dexscreener.com/solana/8jzua3cmwd8dbwxcyp1d9nnsonwqbrh8jstwk2gfpwja"
   },
   {
     name: "URANUS",
-    ticker: "URANUS",
-    marketCap: "482M",
+    symbol: "URANUS",
+    marketcap: "482",
     change: 10,
-    imageUrl: "/uranus.jpg",
-    dexUrl: "https://dexscreener.com/solana/9NRifL3nKQU84hMTbhE7spakkGy5vq4AvNHNQYr8LkW7"
+    price: 324141,
+    logoURI: "/uranus.jpg",
+    address: "https://dexscreener.com/solana/9NRifL3nKQU84hMTbhE7spakkGy5vq4AvNHNQYr8LkW7"
   },
   {
     name: "TROLL",
-    ticker: "TROLL",
-    marketCap: "19M",
+    symbol: "TROLL",
+    marketcap: "19",
     change: -11,
-    imageUrl: "/troll.jpg",
-    dexUrl: "https://dexscreener.com/solana/4a2cmciho17cvgnd11vtk7kb7uwafj9ho2nwkc77eyzt"
+    price: 324141,
+    logoURI: "/troll.jpg",
+    address: "https://dexscreener.com/solana/4a2cmciho17cvgnd11vtk7kb7uwafj9ho2nwkc77eyzt"
   },
   {
     name: "FART",
-    ticker: "FART",
-    marketCap: "1.7B",
+    symbol: "FART",
+    marketcap: "1.7",
+    price: 324141,
     change: -3,
-    imageUrl: "/fartcoin.jpg",
-    dexUrl: "https://dexscreener.com/solana/bzc9nzfmqkxr6fz1dbph7bdf9broyef6pnzesp7v5iiw"
+    logoURI: "/fartcoin.jpg",
+    address: "https://dexscreener.com/solana/bzc9nzfmqkxr6fz1dbph7bdf9broyef6pnzesp7v5iiw"
   }
 ];
