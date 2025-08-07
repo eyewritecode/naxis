3fc0f96be53ab3c24d1cb07835db9c9a0db2ed0c
Eyewritecode <933kss@gmail.com>
2025-08-08 02:51:22 +0900

    update component with new birdeye attributes

--- components/memecoins/memecoinCard.tsx
+++ components/memecoins/memecoinCard.tsx
@@ -7,7 +7,7 @@ const MemecoinCard = (coin: Memecoin) => {
     <div className="flex items-center justify-between px-4 py-3 rounded-xl bg-white/5 w-full max-w-xs md:max-w-sm">
       <div className="w-10 h-10 flex-shrink-0 rounded-full overflow-hidden bg-white/10">
         <Image
-          src={coin.imageUrl}
+          src={coin.logoURI}
           alt={coin.name}
           width={20}
           height={20}
@@ -16,7 +16,7 @@ const MemecoinCard = (coin: Memecoin) => {
       </div>
 
       <div className="flex flex-col text-left flex-grow">
-        <span className="text-sm font-semibold">{coin.ticker.toUpperCase()}</span>
+        <span className="text-sm font-semibold">{coin.symbol.toUpperCase()}</span>
         <span className="text-xs text-white/60">{coin.name}</span>
       </div>
 
@@ -27,10 +27,10 @@ const MemecoinCard = (coin: Memecoin) => {
           }`}
         >
           {coin.change >= 0 ? '+' : ''}
-          {coin.change.toFixed(1)}%
+          {coin.change}%
         </div>
         <div className="text-xs text-white/50">
-          {coin.marketCap ?? '$—'}
+          {coin.marketcap ?? '$—'}
         </div>
       </div>
     </div>
--- components/memecoins/memecoinList.tsx
+++ components/memecoins/memecoinList.tsx
@@ -1,12 +1,18 @@
-import { sampleCoins } from '@/data/sampleCoins'
+'use client'
+
+import { useMemecoin } from '@/hooks/useMemecoin'
 import React from 'react'
 import MemecoinCard from './memecoinCard'
 
 const MemecoinList = () => {
+  const { data: tokens, loading } = useMemecoin()
+
+  if (loading) return <p>Loading...</p>
+
   return (
     <section className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-      {sampleCoins.map(coin =>
-        <MemecoinCard key={coin.ticker} {...coin} />
+      {tokens.map(coin =>
+        <MemecoinCard key={coin.symbol} {...coin} />
       )}
     </section>
   )
